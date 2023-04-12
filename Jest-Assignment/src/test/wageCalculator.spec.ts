79b7a9e829dd0fc69a1e8c2c9e6f66b9d8310fab
Nick <nick.rattanaton@gmail.com>
2023-04-12 14:38:27 +0700

    👩‍💻 wageCalculator.ts test 100% branch coverage

--- Jest-Assignment/src/test/wageCalculator.spec.ts
+++ Jest-Assignment/src/test/wageCalculator.spec.ts
@@ -1,7 +1,21 @@
 import { wageCalculator } from "../wageCalculator";
 
 describe("wageCalculator", () => {
-  it("Case 1: Not a leap year, mon, wed, no work level", () => {
-    expect(wageCalculator(2023, ["mon", "wed"])).toStrictEqual(170);
+  it("Case 1: Not a leap year, mon, tue, wed, thu, sat, sun no work level", () => {
+    expect(
+      wageCalculator(2023, ["mon", "tue", "wed", "thu", "sat", "sun"])
+    ).toStrictEqual(720);
+  });
+
+  it("Case 2: Not a leap year, tue with director work level", () => {
+    expect(wageCalculator(2023, ["tue"], "director")).toStrictEqual(110);
+  });
+
+  it("Case 3: Not a leap year, tue, fri with manager work level", () => {
+    expect(wageCalculator(2023, ["tue", "fri"], "manager")).toStrictEqual(180);
+  });
+
+  it("Case 4: Leap year, fri, invalidDay with no work level", () => {
+    expect(wageCalculator(2020, ["fri", "invalidDay"])).toStrictEqual(252);
   });
 });
