3f3afffea0b6f3aedeb74c74663db702cb35c31a
Nick <nick.rattanaton@gmail.com>
2023-04-12 14:20:56 +0700

    👩‍💻 Add 100% coverage test for passwordSafety.ts

--- Jest-Assignment/src/test/passwordSafety.spec.ts
+++ Jest-Assignment/src/test/passwordSafety.spec.ts
@@ -6,4 +6,34 @@ describe("passwordSafety", () => {
       "Password should more than 8 characters"
     );
   });
+
+  it("Case 2: Password does not contain at least 1 number", () => {
+    expect(passwordSafety("pppppppp")).toStrictEqual(
+      "Password should contain at least 1 number"
+    );
+  });
+
+  it("Case 3: Password does not contain at least 1 uppercase character", () => {
+    expect(passwordSafety("pppppppp1")).toStrictEqual(
+      "Password should contain at least 1 uppercase character"
+    );
+  });
+
+  it("Case 4: Password does not contain at least 1 lowercase character", () => {
+    expect(passwordSafety("PPPPPPPP1")).toStrictEqual(
+      "Password should contain at least 1 lowercase character"
+    );
+  });
+
+  it("Case 5: Password does not contain at least 1 special character", () => {
+    expect(passwordSafety("PPPPPPPP1a")).toStrictEqual(
+      "Password should contain at least 1 special character"
+    );
+  });
+
+  it("Case 6: Password is safe (a little bit)", () => {
+    expect(passwordSafety("PPPPPPPP1a!")).toStrictEqual(
+      "Password is safe (a little bit)"
+    );
+  });
 });
